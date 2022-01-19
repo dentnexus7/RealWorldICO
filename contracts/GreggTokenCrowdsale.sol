5f440a5474ea42d9deff4a2443df1568d745ec17
Gregg Dent <71772319+dentnexus7@users.noreply.github.com>
2022-01-19 09:48:55 -0700

    Minted Crowdsale & Capped Crowdsale

--- contracts/GreggTokenCrowdsale.sol
+++ contracts/GreggTokenCrowdsale.sol
@@ -1,13 +1,43 @@
 pragma solidity 0.4.24;
 
-import "openzeppelin-solidity/contracts/crowdsale/CrowdSale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract GreggTokenCrowdsale is Crowdsale {
+contract GreggTokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
 
-    constructor(uint256 _rate, address _wallet, ERC20 _token) 
-        Crowdsale(_rate, _wallet, _token)    
+    // Track investor contributions
+    uint256 public investorMinCap = 2000000000000000; // 0.002 Ether
+    uint256 public investorMaxCap = 50000000000000000000; // 50 Ether
+    mapping(address => uint256) public contributions;
+
+    constructor(uint256 _rate, address _wallet, ERC20 _token, uint256 _cap) 
+        Crowdsale(_rate, _wallet, _token)
+        CappedCrowdsale(_cap)    
         public 
     {
 
+    }
+
+    /**
+    * @dev Returns the amount contributed so far by a specific user.
+    * @param _beneficiary Address of contributor
+    * @return User contribution so far
+    */
+    function getUserContribution(address _beneficiary) public view returns (uint256) {
+        return contributions[_beneficiary];
+    }
+
+    /**
+    * @dev Extend parent behavior requiring purchase to respect investor min/max funding cap
+    * @param _beneficiary Token purchaser
+    * @param _weiAmount Amount of wei contributed
+    */
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        super._preValidatePurchase(_beneficiary, _weiAmount);
+        uint256 _existingContribution = contributions[_beneficiary];
+        uint256 _newContribution = _existingContribution.add(_weiAmount);
+        require(_newContribution >= investorMinCap && _newContribution <= investorMaxCap);
+        contributions[_beneficiary] = _newContribution;
     }
 }
--- test/GreggTokenCrowdsale.test.js
+++ test/GreggTokenCrowdsale.test.js
@@ -1,15 +1,19 @@
+import ether from './helpers/ether';
+import EVMRevert from './helpers/EVMRevert';
+
 const { assert } = require('chai');
 
-const BigNumber = web3.BigNumber;
+const BigNumber = web3.utils.BN;
 
 const GreggToken = artifacts.require('GreggToken');
 const GreggTokenCrowdsale = artifacts.require("GreggTokenCrowdsale");
 
 require('chai')
+  .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(BigNumber))
   .should();
   
-contract('GreggTokenCrowdsale', function([_, wallet]) {
+contract('GreggTokenCrowdsale', function([_, wallet, investor1, investor2]) {
 
     beforeEach(async function() {
         // Token config
@@ -17,12 +21,23 @@ contract('GreggTokenCrowdsale', function([_, wallet]) {
         this.symbol = "GGG";
         this.decimals = 18;
 
+        // Deploy token
         this.token = await GreggToken.new(this.name, this.symbol, this.decimals);
 
+        // Crowdsale config
         this.rate = 500;
         this.wallet =wallet;
+        this.cap = ether('100');
 
-        this.crowdsale = await GreggTokenCrowdsale.new(this.rate, this.wallet, this.token.address);
+        // Investor caps
+        this.investorMinCap = ether('0.002');
+        this.investorMaxCap = ether('50');
+
+        // Crowdsale deploy
+        this.crowdsale = await GreggTokenCrowdsale.new(this.rate, this.wallet, this.token.address, this.cap);
+
+        // Transfer token ownership to crowdsale
+        await this.token.transferOwnership(this.crowdsale.address);
     });
 
     describe('crowdsale', function() {
@@ -41,5 +56,73 @@ contract('GreggTokenCrowdsale', function([_, wallet]) {
             const token = await this.crowdsale.token();
             token.should.equal(this.token.address);
         });
-    })
+    });
+
+    describe('minted crowdsale', function() {
+      it('mints tokens after purchase', async function() {
+        const originalTotalSupply = await this.token.totalSupply();
+        await this.crowdsale.sendTransaction({ value: ether('1'), from: investor1 });
+        const newTotalSupply = await this.token.totalSupply();
+        assert.isTrue(newTotalSupply > originalTotalSupply);
+      });
+    });
+
+    describe('capped crowdsale', function() {
+      it('has the correct hard cap', async function() {
+        const cap = await this.crowdsale.cap();
+        assert.equal(cap.toString(), this.cap.toString(), "cap is correct");
+        //cap.should.be.bignumber.equal(this.cap);
+      });
+    });
+
+    describe('accepting payments', function() {
+      it('should accept payments', async function() {
+        const value = ether('1');
+        const purchaser = investor2;
+        await this.crowdsale.sendTransaction({ value: value, from: investor1 }).should.be.fulfilled;
+        await this.crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
+      });
+    });
+
+    describe('buyTokens()', function() {
+      describe('when the contribution is less than the minimum cap', function() {
+        it('rejects the transaction', async function() {
+          const value = this.investorMinCap - 1;
+          await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.rejectedWith(EVMRevert);
+        });
+      });
+
+       describe('when the investor has already met the minimum cap', function() {
+        it('allows the investor to contribute below the minimum cap', async function() {
+          // First contribution is valid
+          const value1 = ether('1');
+          await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+          // Second contribution is less than minimum investor cap
+          const value2 = 1; // wei
+          await this.crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.fulfilled;
+        });
+      });
+
+      describe('when the total contributions exceeds the investor maximum cap', function() {
+        it('rejects the transaction', async function() {
+          // First contribution is valid
+          const value1 = ether('2');
+          await this.crowdsale.buyTokens(investor1, { value: value1, from: investor1 });
+          // Second contribution is less than minimum investor cap
+          const value2 = ether('49');
+          await this.crowdsale.buyTokens(investor1, { value: value2, from: investor1 }).should.be.rejectedWith(EVMRevert);
+        });
+      });
+
+      describe('when the contribution is within the valid range', function () {
+        const value = ether('2');
+        it('succeeds & updates the contribution amount', async function () {
+          await this.crowdsale.buyTokens(investor2, { value: value, from: investor2 }).should.be.fulfilled;
+          const contribution = await this.crowdsale.getUserContribution(investor2);
+          assert.equal(contribution.toString(), value.toString(), "contribution is correct");
+          //contribution.should.be.bignumber.equal(value);
+        });
+      });
+
+    });
 });
